7356aefdc46f939a18c8ea4ce3fc9363fb20197b
vlasted <167018199+vlasted@users.noreply.github.com>
2026-04-23 19:17:03 +0200

    añadida segunda query, se testea pero en la version small el total_amount es 915250.00

--- IntroBBDD.sql
+++ IntroBBDD.sql
@@ -2,11 +2,17 @@
 -- Autor: Lenin Fonseca
 
 -- Consulta 1
+
 SELECT flight_id, status
 FROM flights
 WHERE status = 'On Time';
 
 -- Consulta 2
+-- En esta versión small de la base demo no devuelve filas porque el valor máximo de total_amount es 915250.00.
+
+SELECT *
+FROM bookings
+WHERE total_amount > 1000000;
 
 
 -- Consulta 3
